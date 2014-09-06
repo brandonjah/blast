719b315c8814103f9f2d3798084e63647ff3954b
brandonjah <brandon.jahner@gmail.com>
2014-09-06 11:54:56 -0700

    updating user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,34 @@
 class SessionsController < ApplicationController
-  def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
-  end
+  # def create
+  #   user = User.from_omniauth(env["omniauth.auth"])
+  #   session[:user_id] = user.id
+  #   redirect_to root_url, notice: "Signed in!"
+  # end
   
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, notice: "Signed out!"
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_url, notice: "Signed out!"
+  # end
+  def new
+		redirect_to '/auth/twitter'
+	end
+
+	def create
+		puts User.inspect
+		auth = request.env["omniauth.auth"]
+		user = User.where(:provider => auth['provider'],
+		:uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+		reset_session
+		session[:user_id] = user.id
+		redirect_to root_url, :notice => 'Signed in!'
+	end
+
+	def destroy
+		reset_session
+		redirect_to root_url, :notice => 'Signed out!'
+	end
+
+	def failure
+		redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+	end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+  
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+    end
+  end
+
+  def self.create_with_omniauth(auth)
+    puts "self::::::::"+self.inspect
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+      end
+    end
+  end  
 end
