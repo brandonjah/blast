9dbb84c8aba0bffeabeb4ecbbf6ae1e9beb864cb
brandonjah <brandon.jahner@gmail.com>
2014-10-20 18:18:32 -0700

    format date in model

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,8 @@ class Content < ActiveRecord::Base
 	has_many :tweets
 	validates :post, length: {minimum: 5, maximum: 140}
   validates_format_of :name, with: /([A-Za-z0-9\-\_]+)/i
+
+  def pretty_date
+  	post_time.strftime("%m/%d/%Y at %I:%M%p") if post_time
+  end
 end
