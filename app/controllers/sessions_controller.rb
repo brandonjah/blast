4254540ccb18265fb7191af921a8b4135f1c3208
brandonjah <brandon.jahner@gmail.com>
2014-10-22 16:13:23 -0700

    removing alerts, landing page html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     # redirect_to root_url, notice: "Signed in!"
-    redirect_to request.env['omniauth.origin'] || root_url, notice: "Signed in!"
+    redirect_to request.env['omniauth.origin'] || root_url#, notice: "Signed in!"
   end
   
   def new
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
 	def destroy
 		reset_session
-		redirect_to root_url, :notice => 'Signed out!'
+		redirect_to request.env['omniauth.origin'] || root_url#, :notice => 'Signed out!'
 	end
 
 	def failure
