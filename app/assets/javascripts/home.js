f81ce4f9aa41b761669cd1d0153758b443a11870
brandonjah <brandon.jahner@gmail.com>
2014-10-29 15:56:29 -0700

    adding mailchimp signup modal

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,4 +4,11 @@ $(function(){
 		$("#example-content").toggle();
 		return false;
 	});
+	$("#cta a").click(function(){
+		$("#signup-modal").modal("show");
+		return false;
+	});
+	$("#mc-embedded-subscribe").click(function(){
+		$("#signup-modal").modal("hide");
+	})
 });
