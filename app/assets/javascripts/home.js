9ab1fe40e2c3108fd239018d931ee60c9abdc616
brandonjah <brandon.jahner@gmail.com>
2014-11-03 17:15:57 -0800

    hiding unfinished functionality on home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,14 @@
-$(function(){
-	$(".example a").click(function(){
-		$(this).hide();
-		$("#example-content").toggle();
-		return false;
-	});
-	$("#cta a").click(function(){
-		$("#signup-modal").modal("show");
-		return false;
-	});
-	$("#mc-embedded-subscribe").click(function(){
-		$("#signup-modal").modal("hide");
-	})
-});
+// $(function(){
+// 	$(".example a").click(function(){
+// 		$(this).hide();
+// 		$("#example-content").toggle();
+// 		return false;
+// 	});
+// 	$("#cta a").click(function(){
+// 		$("#signup-modal").modal("show");
+// 		return false;
+// 	});
+// 	$("#mc-embedded-subscribe").click(function(){
+// 		$("#signup-modal").modal("hide");
+// 	})
+// });
