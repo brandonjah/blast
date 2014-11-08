aa8024c5b6a484cf9c15687ab31941699e6604fa
brandonjah <brandon.jahner@gmail.com>
2014-11-07 16:56:08 -0800

    adding heirs stylesheet

---
+++ public/assets/home-98c537e7487aaccbcb63a5fab634a985.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(".example a").click(function(){
+		$(this).hide();
+		$("#example-content").toggle();
+		return false;
+	});
+	$("#cta a").click(function(){
+		$("#signup-modal").modal("show");
+		return false;
+	});
+	$("#mc-embedded-subscribe").click(function(){
+		$("#signup-modal").modal("hide");
+	})
+});
