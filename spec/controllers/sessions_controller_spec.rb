af411d145897b56bbf69161fa871e8b022cc3147
brandonjah <brandon.jahner@gmail.com>
2014-11-10 10:47:29 -0800

    adding test files

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,53 @@
+# sessions_controller_spec.rb
+require 'spec_helper'
+require 'rails_helper'
+ 
+describe SessionsController do
+ 
+  before :each do
+    request.env['omniauth.auth'] = OmniAuth::AuthHash.new({ :provider => 'twitter', :uid => '1235456' })
+  end
+ 
+  describe "#create" do
+
+    it "should successfully create a user" do
+    #   # visit '/auth/twitter'
+    #   expect {
+    #     post :create
+    #   }.to change{ User.count }.by(1)
+      user_count = User.count
+      post :create
+      expect(user_count).to be > User.count
+    end
+
+    # it "should successfully create a session" do
+    #   session[:user_id].should be_nil
+    #   post :create, provider: :twitter
+    #   session[:user_id].should_not be_nil
+    # end
+ 
+    # it "should redirect the user to the root url" do
+    #   post :create, provider: :twitter
+    #   response.should redirect_to root_url
+    # end
+ 
+  end
+ 
+  # describe "#destroy" do
+  #   before do
+  #     post :create, provider: :twitter
+  #   end
+ 
+  #   it "should clear the session" do
+  #     session[:user_id].should_not be_nil
+  #     delete :destroy
+  #     session[:user_id].should be_nil
+  #   end
+ 
+  #   it "should redirect to the home page" do
+  #     delete :destroy
+  #     response.should redirect_to root_url
+  #   end
+  # end
+ 
+end
---
+++ spec/features/user_twitter_sign_in_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature 'Sign in via Twitter' do
+
+	# scenario do
+	# 	user_count = User.count
+	# 	visit '/auth/twitter'
+	# 	expect(user_count).to be > User.count
+	# end
+
+end
