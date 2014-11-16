cd680aff83a6d80a243b428960b1e6f3f4946243
brandonjah <brandon.jahner@gmail.com>
2014-11-16 13:09:22 -0800

    combining custom text and fixed text for tweet message

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -1,4 +1,7 @@
 $(function(){
+	var content = $("#combined_tweet").text().trim();
+	var tm = $("#tweet_message");
+	tm.attr("maxlength",(140-content.length));
 	$("#launch_tweet_modal_button").click(function(){
 		$('#send-tweet-modal').modal("show");
 		return false;
@@ -6,4 +9,12 @@ $(function(){
 	if($("#show_modal").text().trim() == "true") {
 		$('#send-tweet-modal').modal("show");
 	}
+	tm.keypress(function(){
+		var new_text = $(this).val();
+		var combined = new_text+" "+content;
+		$("#combined_tweet").text(combined);
+	});
+	$("#schedule_tweet_button").click(function(){
+		tm.val($("#combined_tweet").text().trim());
+	});
 });
